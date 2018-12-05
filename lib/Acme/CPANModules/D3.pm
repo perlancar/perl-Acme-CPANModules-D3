12ddc49f7a528fec9d90c18da53f20a56fc0ac71
perlancar (on pc-office) <perlancar@gmail.com>
2018-12-05 18:00:28 +0700

    Initial commit

---
+++ lib/Acme/CPANModules/D3.pm
@@ -0,0 +1,17 @@
+package Acme::CPANModules::D3;
+
+# DATE
+# VERSION
+
+our $LIST = {
+    summary => "Modules related to D3.js",
+    entries => [
+        {module=>'Graph::D3'},
+        {module=>'Web::Library::D3'},
+    ],
+};
+
+1;
+# ABSTRACT:
+
+=head1 DESCRIPTION
